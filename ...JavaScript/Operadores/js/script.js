b794e75edaa0c0f71a025351d69bdbb8f5c26180
Rafael Amaral <[ramaral.devmaua@gmail.com]>
2026-07-05 23:24:57 -0300

    início de js até strings

---
+++ ...JavaScript/Operadores/js/script.js
@@ -0,0 +1,28 @@
+// 1) number
+
+console.log(2);
+console.log(typeof 2);
+console.log(typeof 3.14);
+console.log(typeof -5);
+
+// 2) operações matemáticas
+
+console.log(2 + 4);
+console.log(7-2);
+console.log(3 * 4);
+console.log(8 / 2);
+
+console.log(5 * (4 + 2))
+
+// 3) special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof 5 * true)
+console.log(typeof NaN)
+
+// 4) strings
+
+console.log("um texto")
+console.log('mais um texto')
+console.log("12")
+console.log(typeof "12")
